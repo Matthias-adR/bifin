3721fed2bd868983c51e42986a0a33c5b05a9d59
Matthias an der Ruhr <135343836+Matthias-adR@users.noreply.github.com>
2025-10-24 17:34:39 +0200

    fix material-symbols-fonts

--- build_files/build.sh
+++ build_files/build.sh
@@ -28,13 +28,16 @@ dnf5 -y --enablerepo copr:copr.fedorainfracloud.org:zirconium:packages install \
     matugen \
     cliphist
 
+dnf5 -y copr enable purian23/material-symbols-fonts
+dnf5 -y copr disable purian23/material-symbols-fonts
+dnf5 -y --enablerepo copr:copr.fedorainfracloud.org:purian23:material-symbols-fonts install material-symbols-fonts
+
 mkdir ~/.config/quickshell && git clone https://github.com/AvengeMedia/DankMaterialShell.git ~/.config/quickshell/dms
 sh -c "curl -L https://github.com/AvengeMedia/danklinux/releases/latest/download/dms-$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/').gz | gunzip | tee /usr/local/bin/dms > /dev/null && chmod +x /usr/local/bin/dms"
 
 dnf5 -y copr enable avengemedia/danklinux
 dnf5 -y copr disable avengemedia/danklinux
 dnf5 -y --enablerepo copr:copr.fedorainfracloud.org:avengemedia:danklinux install \
-     material-symbols-fonts \
      hyprpicker \
      dgop \
      brightnessctl \
