70770efa3120b7a5436204dacc75a7318e876eef
Matthias an der Ruhr <135343836+Matthias-adR@users.noreply.github.com>
2025-10-27 20:35:17 +0100

    retry mullvad vpn

--- build_files/build.sh
+++ build_files/build.sh
@@ -172,10 +172,10 @@ dnf5 -y install \
      glib2-devel
 
 
-# mullvad vpn (disabled for now)
-#dnf5 -y config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo
-#dnf5 -y --enablerepo=mullvad install mullvad-vpn
-#dnf5 -y config-manager --set-disabled mullvad
+# mullvad vpn
+dnf5 -y config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo
+dnf5 -y --enablerepo=mullvad install mullvad-vpn
+dnf5 -y config-manager --set-disabled mullvad
 
 
 # zirconium stuff i yanked
