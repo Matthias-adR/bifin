6a7e2f36acc756fe84fa87aa581be85360e064c8
Matthias an der Ruhr <135343836+Matthias-adR@users.noreply.github.com>
2025-11-01 18:59:16 +0100

    rollback cachyos kernal add

--- build_files/build.sh
+++ build_files/build.sh
@@ -211,21 +211,6 @@ systemctl enable --global dms.service
 #systemctl enable --global udiskie.service
 systemctl enable --global xwayland-satellite.service
 
-# cachyos kernel
-dnf update
-rm -r -f /usr/lib/modules
-dnf -y copr enable bieszczaders/kernel-cachyos
-dnf -y install kernel-cachyos
-dnf -y copr enable bieszczaders/kernel-cachyos-addons
-dnf -y swap zram-generator-defaults cachyos-settings
-dnf -y install scx-scheds-git
-dnf -y install scx-manager
-
-KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
-export DRACUT_NO_XATTR=1
-dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
-chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
-
 
 # fonts
 dnf5 -y install \
