4e0cb8c9fae9351838876636d0dd0e3db5e543d4
Matthias an der Ruhr <135343836+Matthias-adR@users.noreply.github.com>
2025-11-02 23:31:46 +0100

    undo cachyos attempt

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,48 +3,6 @@
 set -ouex pipefail
 cp -avf /ctx/files/. /
 
-# cachyos kernel attempt
-for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
-  rpm --erase $pkg --nodeps
-done
-
-pushd /usr/lib/kernel/install.d
-printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
-printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
-chmod +x  05-rpmostree.install 50-dracut.install
-popd
-
-dnf -y copr enable bieszczaders/kernel-cachyos-lto
-dnf -y copr disable bieszczaders/kernel-cachyos-lto
-dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-lto install \
-  kernel-cachyos-lto
-
-dnf -y copr enable bieszczaders/kernel-cachyos-addons
-dnf -y copr disable bieszczaders/kernel-cachyos-addons
-dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons swap zram-generator-defaults cachyos-settings
-dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons install \
-  scx-scheds-git \
-  cachyos-ksm-settings \
-  scx-manager
-
-# TODO: remove once https://github.com/CachyOS/copr-linux-cachyos/pull/69 is merged
-install -Dpm0644 -t /usr/lib/systemd/system/greetd.service.d /usr/lib/systemd/system/user@.service.d/10-ksm.conf
-
-tee /usr/lib/tmpfiles.d/10-piperita.conf <<'EOF'
-w! /sys/kernel/mm/transparent_hugepage/shmem_enabled - - - - advise
-w! /sys/kernel/mm/ksm/sleep_millisecs - - - - 500
-EOF
-
-tee /usr/lib/modules-load.d/piperita-ntsync.conf <<'EOF'
-ntsync
-EOF
-
-KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
-export DRACUT_NO_XATTR=1
-dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
-chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
-
-
 # COPRs, DMS
 
 dnf -y copr enable yalter/niri-git
